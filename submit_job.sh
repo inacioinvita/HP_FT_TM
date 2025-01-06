5c0326392bede0f3ad5e1bb5c4db8e4dccc50f4f
Inacio Vieira <inacio@gmail.com>
2025-01-06 16:37:45 +0000

    Updated to latest version

--- submit_job.sh
+++ submit_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Convert line endings to Unix format
-sed -i 's/\r$//' run_sage_train_inf_eval.job
+sed -i 's/\r$//' run_BALS_trainer.job
 
 # Submit the job
-sbatch run_sage_train_inf_eval.job
+sbatch run_BALS_trainer.job
