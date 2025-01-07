b41e1b2875adabba86e7024cf7f91deffbec733d
Inacio Vieira <inacio@gmail.com>
2025-01-07 10:28:33 +0000

    Updated to latest version

--- submit_job.sh
+++ submit_job.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 4) Pull the latest changes from the repo
+# git fetch origin
+# git checkout origin/main -- run_BALS_trainer.job BALS_trainer.py
+git pull origin main
+
 # Convert line endings to Unix format
 sed -i 's/\r$//' run_BALS_trainer.job
 
